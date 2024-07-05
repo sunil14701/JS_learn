f928cdf1dd2e50b4214cf8fdff3971e7f0a85fd8
Sunil.A.Kumar <Sunil.A.Kumar@Geminisolutions.com>
2024-07-05 08:02:41 +0530

    first commit

---
+++ Sec_2_JS_fundamentals_P1/1_browser_console.js
@@ -0,0 +1,23 @@
+// in browser
+
+alert("Hello World!!"); // pop up window in chrome
+
+var j = "amazing";
+if (j === "amazing") alert("JS is FUN!"); //-> pop up will show
+js = "boring";
+if (j === "amazing") alert("JS is FUN!"); //-> pop up will not show
+if (j === "boring") alert("JS is FUN!"); //-> pop up will show
+
+// 2+34+10 -> calc ans
+
+// 2. multi-paradigm -> we can use diff styles of programming such as imperative and declarative programming
+
+// 3. core tech of web
+// HTML(content), css(styling)
+// JS-> dynamic and interactive effects to webpage, manuplate html and css, load data from servers, build web apps
+
+// 4. learning JS properly is best investment as react, angular, vue frameworks are based on JS.
+
+// 5. browser and JS are two seperate things, JS can run outside the browser i.e on server/web-server using node.js and can interact with databases called backend applications. when JS in browser than that is called frontend applications.
+
+// 6. native mobile apps and native desktop apps can be made using JS
